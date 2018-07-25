778510ca78cdbf7608f32e595b3162512d5b4554
dembasy <dembasy1227@gmail.com>
2018-07-25 16:24:04 +0200

    done

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8" />
-    <title>ex5</title>
+    <title>ex6</title>
   </head>
   <body>
     <p><?php $name = "Demba"; echo 'Bonjour '.$name. ' comment vas tu ? '; ?></p>
